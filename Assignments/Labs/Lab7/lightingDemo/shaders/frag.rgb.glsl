ab46d3047d247a688d8bdb7bcaac6a00ffd1a29a
Keith Skinner <brian.skinner121@myci.csuci.edu>
2019-10-23 18:22:48 -0700

    Lighting Change

--- Assignments/Labs/Lab7/lightingDemo/shaders/frag.rgb.glsl
+++ Assignments/Labs/Lab7/lightingDemo/shaders/frag.rgb.glsl
@@ -42,7 +42,32 @@ varying vec3 fragNormal;
 
 void main()
 {
-	// TODO decide what color the fragment should be, assign to gl_FragColor
+	// TODO decide what color the fragment should be, assign to gl_FragColor	
+	vec3 ambient = ambientLight;
+	vec3 diffuse = vec3(0.0, 0.0, 0.0);
+	vec3 specular = vec3(0.0, 0.0, 0.0);
 
-	gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+	for (int i = 0; i < 16; i++)
+	{
+		ambient += directionalLights[i].ambient;
+		ambient += pointLights[i].ambient;
+
+		vec3 Sl = -directionalLights[i].direction;
+		diffuse += max(dot(Sl, fragNormal), 0.0) * directionalLights[i].diffuse;
+
+		vec3 Rl = reflect(Sl, fragNormal);
+		vec3 V = normalize(cameraPosition - fragPosition);
+		specular += pow(max(dot(Rl, V), material.shininess), 0.0) * directionalLights[i].specular;
+
+		Sl = normalize(pointLights[i].position - fragPosition);
+		float rad = length(pointLights[i].position - fragPosition);
+		diffuse += max(dot(Sl, fragNormal), 0.0) * pointLights[i].diffuse / (rad * rad + 1.0); 
+
+		Rl = reflect(Sl, fragNormal);
+		specular += pow(max(dot(Rl, V), material.shininess), 0.0) * pointLights[i].specular / (rad * rad + 1.0);
+	}
+
+	vec3 light = (material.ambient * ambient) + (material.diffuse * diffuse) + (material.specular * specular);
+
+	gl_FragColor = vec4(light, 1.0);
 }
